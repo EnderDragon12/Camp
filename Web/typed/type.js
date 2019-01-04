bf826647370dfbcaa1605d7e84b464875a552344
EnderDragon12 <46386483+EnderDragon12@users.noreply.github.com>
2019-01-04 13:54:27 -0800

    Add files via upload

---
+++ Web/typed/type.js
@@ -0,0 +1,73 @@
+jQuery(document).ready(function() {
+  var type = new Typed('#type', {
+    strings: [
+      'I',
+      'suck',
+      'At Fortnite'
+    ],
+    typeSpeed: 0,
+    backSpeed: 0,
+    backDelay: 1500,
+    loop: true,
+  });
+  var title1 = new Typed('#cardTitle1', {
+    strings: [
+      'pixal gun 3d',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title2 = new Typed('#cardTitle2', {
+    strings: [
+      'Let\'s go play fruit ninja',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title3 = new Typed('#cardTitle3', {
+    strings: [
+      'click to go to Youtube',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title4 = new Typed('#cardTitle4', {
+    strings: [
+      'ice king skin',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title5 = new Typed('#cardTitle5', {
+    strings: [
+      'Ugly Elmo',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title6 = new Typed('#cardTitle6', {
+    strings: [
+      'I dropped a picture of my wii remote',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+});
